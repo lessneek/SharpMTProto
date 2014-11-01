dc955b18f3456b42c94ad7d1975dc36d3e430dc1
Alexander Logger <intagger@live.ru>
2014-11-02 00:26:56 +0300

    Add helper: method for cloning of TLObjects.

---
+++ src/SharpMTProto/SharpMTProto.Tests.NET45/Extensions.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Extensions.cs">
+//   Copyright (c) 2013-2014 Alexander Logger. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using SharpTL;
+
+namespace SharpMTProto.Tests
+{
+    public static class Extensions
+    {
+        public static T CloneTLObject<T>(this T obj)
+        {
+            byte[] bytes = TLRig.Default.Serialize(obj);
+            var clone = TLRig.Default.Deserialize<T>(bytes);
+            return clone;
+        }
+    }
+}
