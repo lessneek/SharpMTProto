7b37409623357732ba314a9483281c8a5d82d263
Alexander Logger <intagger@live.ru>
2014-11-23 16:13:18 +0300

    Renamed CommonAssemblyInfo > AssemblyInfo.

--- src/SharpMTProto.Shared/AssemblyInfo.cs
+++ src/SharpMTProto.Shared/AssemblyInfo.cs
@@ -15,4 +15,7 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("0.6.0")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
