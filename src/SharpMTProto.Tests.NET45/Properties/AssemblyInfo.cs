4321d3b86568674d89a6c83993bfd71396b51a4c
Alexander Logger <intagger@live.ru>
2014-11-23 16:14:10 +0300

    Added tests assembly version info.

--- src/SharpMTProto.Tests.NET45/Properties/AssemblyInfo.cs
+++ src/SharpMTProto.Tests.NET45/Properties/AssemblyInfo.cs
@@ -8,3 +8,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("SharpMTProto.Tests.NET45")]
 [assembly: AssemblyDescription("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
