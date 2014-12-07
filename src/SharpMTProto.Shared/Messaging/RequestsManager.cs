4ca3b0a2469a0280f255d4198122b3b509a1b11f
Alexander Logger <intagger@live.ru>
2014-12-07 16:32:11 +0300

    RequestsManager: added thread-safety.

--- src/SharpMTProto.Shared/Messaging/RequestsManager.cs
+++ src/SharpMTProto.Shared/Messaging/RequestsManager.cs
@@ -4,17 +4,18 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
-using System.Linq;
-
 namespace SharpMTProto.Messaging
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public interface IRequestsManager
     {
         void Add(IRequest request);
         IRequest Get(ulong messageId);
         IRequest GetFirstOrDefault(object response, bool includeRpc = false);
         void Change(ulong newMessageId, ulong oldMessageId);
+        void Remove(ulong messageId);
     }
 
     public class RequestsManager : IRequestsManager
@@ -23,29 +24,46 @@ namespace SharpMTProto.Messaging
 
         public void Add(IRequest request)
         {
-            _requests.Add(request.Message.MsgId, request);
+            lock (_requests)
+            {
+                _requests.Add(request.Message.MsgId, request);
+            }
         }
 
         public void Change(ulong newMessageId, ulong oldMessageId)
         {
-            _requests.Add(newMessageId, _requests[oldMessageId]);
-            _requests.Remove(oldMessageId);
+            lock (_requests)
+            {
+                _requests.Add(newMessageId, _requests[oldMessageId]);
+                _requests.Remove(oldMessageId);
+            }
         }
 
         public IRequest Get(ulong messageId)
         {
-            IRequest request;
-            return _requests.TryGetValue(messageId, out request) ? request : null;
+            lock (_requests)
+            {
+                IRequest request;
+                return _requests.TryGetValue(messageId, out request) ? request : null;
+            }
         }
 
         public IRequest GetFirstOrDefault(object response, bool includeRpc = false)
         {
-            return _requests.Values.FirstOrDefault(r => r.CanSetResponse(response) && (!r.Flags.HasFlag(MessageSendingFlags.RPC) || includeRpc));
+            lock (_requests)
+            {
+                return
+                    _requests.Values.FirstOrDefault(
+                        r => r.CanSetResponse(response) && (!r.Flags.HasFlag(MessageSendingFlags.RPC) || includeRpc));
+            }
         }
 
         public void Remove(ulong messageId)
         {
-            _requests.Remove(messageId);
+            lock (_requests)
+            {
+                _requests.Remove(messageId);
+            }
         }
     }
 }
